6023c8aeab2da1b86f83b02055daab97738f52d3
ismilearefin <arefinismile@gmail.com>
2023-11-02 04:05:48 +0600

    Footer design responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-
-import './App.css'
-import Body from './components/Body/Body'
-// import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navbar'
+import "./App.css";
+import Body from "./components/Body/Body";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  
-
   return (
-        <div className='backgroundColor'>
-    <div className='customWidth mx-auto'>
-      <Navbar></Navbar>
-      <Body></Body>
-      {/* <Footer></Footer> */}
+    <div className="backgroundColor">
+      <div className="customWidth mx-auto">
+        <Navbar></Navbar>
+        <Body></Body>
+        <Footer></Footer>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,26 @@
 import '../Footer/Footer.css';
+// import { FaFacebookSquare } from 'react-icons/fa';
+import { AiFillTwitterSquare,AiFillGoogleSquare,AiFillFacebook } from 'react-icons/ai';
 
 export default function Footer() {
   return (
-    <div className='Footer'>
-        <h1>Footer</h1>
+    <div className='Footer p-4 p-md-0 '>
+        <div className='d-flex flex-lg-column align-items-center justify-content-center'>
+          <div className='w-50 d-lg-flex justify-content-evenly'> 
+          <a className='buttonFooter text-white mb-2 d-block'>Audits</a>
+          <a className='buttonFooter text-white d-block mb-2'>Live</a>
+          <a className='buttonFooter text-white d-block mb-2'>WhitePaper</a>
+          </div>
+          <div>
+          <div className='d-flex justify-content-center footerIcons'>
+            <AiFillFacebook/>
+            <AiFillTwitterSquare/>
+            <AiFillGoogleSquare/>
+            <AiFillTwitterSquare/>
+          </div>
+          <p className='text-center fs-4 text-white mt-2 mt-md-5'>© Goose Wealth Creation All Rights reserved</p>
+          </div>
+        </div>
     </div>
   )
 }
